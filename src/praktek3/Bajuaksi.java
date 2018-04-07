6a58c575a289b29ea37c1925ef45b3346902b01f
herlinabjb <herlinabjb46@gmail.com>
2018-04-07 14:19:15 +0700

    instance buat atribut dari panggil mrthod

--- src/praktek3/Bajuaksi.java
+++ src/praktek3/Bajuaksi.java
@@ -2,6 +2,13 @@
 package praktek3;
 public class Bajuaksi {
     public static void main(String[] args) {
-        
+       Baju s = new Baju() ;
+       s.merk="dannis";
+       s.ukuran="xl";
+       s.warna="coklat";
+       s.model="terusan";
+       
+       s.cetakInfo();
+       
     }
 }
