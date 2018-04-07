2a85fd4aba273020e8d8f026495fc1b55a0a15eb
herlinabjb <herlinabjb46@gmail.com>
2018-04-07 14:28:20 +0700

    menggunakan method set

--- src/praktek3/Bajuaksi.java
+++ src/praktek3/Bajuaksi.java
@@ -3,10 +3,11 @@ package praktek3;
 public class Bajuaksi {
     public static void main(String[] args) {
        Baju s = new Baju() ;
-       s.merk="dannis";
-       s.ukuran="xl";
-       s.warna="coklat";
-       s.model="terusan";
+ 
+       s.setMerk("dannis");
+       s.setUkuran("xl");
+       s.setWarna("Hitam");
+       s.setModel("polos");
        
        s.cetakInfo();
        
