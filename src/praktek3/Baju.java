06f2c659cac987f65f67980b8a0f92604c0ba596
herlinabjb <herlinabjb46@gmail.com>
2018-04-07 14:04:14 +0700

    membuat method cetakinfo

--- src/praktek3/Baju.java
+++ src/praktek3/Baju.java
@@ -1,14 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package praktek3;
 
-/**
- *
- * @author lina
- */
 public class Baju {
+    String merk;
+    String ukuran;
+    String warna;
+    String model;
     
-}
+    void cetakInfo(){
+        System.out.println("merk  \t:  "+merk+"\n"+
+        "ukuran \t: "+ukuran+"\n"+
+        "warna \t: "+warna+"\n"+
+        "model \t: "+model);
+        
+    }
+                
+}     
