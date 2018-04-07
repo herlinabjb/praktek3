1a3cdc113a1e80814eac09c4f608dca0bc222389
herlinabjb <herlinabjb46@gmail.com>
2018-04-07 14:37:50 +0700

    atribut dan method

--- src/praktek3/Bajumuslim.java
+++ src/praktek3/Bajumuslim.java
@@ -1,5 +1,9 @@
 
 package praktek3;
-public class Bajumuslim extends baju {
+public class Bajumuslim extends Baju {
+    private String jeniskain;
     
+    void throttle(){
+        System.out.println("ASIKJOSSSSS");
+    }
 }
